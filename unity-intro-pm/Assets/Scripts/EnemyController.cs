93312bac8dee9fec5c9bbe99eb02c5d30272f22d
JTW10 <83363676+JTW-10@users.noreply.github.com>
2024-09-19 12:49:02 -0700

    updated shooting, weapon, dash, armor, and health
    need to change dashing to be a lerp so you don't go through walls. also likely need to change the movement system just a bit on account of sticking to walls and whatnot, unless I turn it into a "feature" rather than it being a bug.

--- unity-intro-pm/Assets/Scripts/EnemyController.cs
+++ unity-intro-pm/Assets/Scripts/EnemyController.cs
@@ -1,22 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.PlasticSCM.Editor.WebApi;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float currentHealth = 100f;
-    public float maxHealth = 100f;
+    public float health = 100;
+    public float maxHealth = 100;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currentHealth <= 0)
+        if (health <= 0)
             Destroy(gameObject);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Shot")
+        {
+            Destroy(other.gameObject);
+            health -= 10;
+        }
+    }
+
 }
--- unity-intro-pm/Assets/Scripts/PlayerController.cs
+++ unity-intro-pm/Assets/Scripts/PlayerController.cs
@@ -24,21 +24,24 @@ public class PlayerController : MonoBehaviour
     public float sprintMultiplier = 1.5f;
     public float jumpHeight = 5f;
     public float groundDetectDistance = 2f;
-    public float dashDistance = 5f;
-    public float dashCooldown = 3f;
     public bool canDash = true;
+    public float dashDistance = 10000f;
+    public float dashCooldown = 3f;
 
     [Header("User Settings")]
-    public float mouseSensitivity = 2f;
-    public float Xsensitivity = 2f;
-    public float Ysensitivity = 2f;
+    public float mouseSensitivity = 3f;
+    public float Xsensitivity = 3f;
+    public float Ysensitivity = 3f;
     public float camRotationLimit = 90f;
     public bool sprintToggleOption = false;
 
     [Header("Player Stats")]
     public float playerMaxHealth = 100f;
     public float playerHealth = 100f;
-    public float pickupHealth = 25f; 
+    public float pickupHealth = 25f;
+    public float hitCooldown = 2f;
+    public float armorCooldown = 10f;
+    public bool canHit = true;
     public bool playerArmor = true;
 
     [Header("Blaster Weapon Stats")]
@@ -78,7 +81,7 @@ public class PlayerController : MonoBehaviour
         if(Input.GetMouseButtonDown(0) && canFire && weaponID >= 0 && currentAmmo > 0)
         {
             GameObject s = Instantiate(shot, weaponSlotBlaster.position, weaponSlotBlaster.rotation);
-            s.GetComponent<Rigidbody>().AddForce(playerCam.transform.forward += Vector3.forward * shotVel);
+            s.GetComponent<Rigidbody>().AddForce(playerCam.transform.forward * shotVel);
             Destroy(s, shotLifespan);
             
             canFire = false;
@@ -90,20 +93,11 @@ public class PlayerController : MonoBehaviour
         {
             isAimed = true;
             playerCam.transform.localPosition += Vector3.forward * 2f;
-            weaponSlotBlaster.transform.localPosition += Vector3.forward * 2f;
         }
         if(Input.GetMouseButtonUp(1))
         {
             isAimed = false;
             playerCam.transform.localPosition += -Vector3.forward * 2f;
-            weaponSlotBlaster.transform.localPosition += -Vector3.forward * 2f;
-        }
-        
-        if(Input.GetKey(KeyCode.LeftAlt))
-        {
-            myRB.AddForce(transform.forward * dashDistance);
-            canDash = false;
-            StartCoroutine("cooldownDash");
         }
 
         Vector3 temp = myRB.velocity;
@@ -128,6 +122,14 @@ public class PlayerController : MonoBehaviour
             if (verticalMove <= 0)
                 sprintMode = false;
         }
+
+        if (Input.GetKey(KeyCode.LeftAlt) && canDash && verticalMove > 0)
+        {
+            myRB.AddForce(transform.forward * dashDistance);
+            canDash = false;
+            StartCoroutine("cooldownDash");
+        }
+
 
         if (!sprintMode)
             temp.x = verticalMove * speed;
@@ -169,7 +171,7 @@ public class PlayerController : MonoBehaviour
 
         if (other.gameObject.tag == "weaponBlaster")
         {
-            other.gameObject.transform.position = weaponSlotBlaster.position;
+            other.gameObject.transform.SetPositionAndRotation(weaponSlotBlaster.position, weaponSlotBlaster.rotation);
 
             other.gameObject.transform.SetParent(weaponSlotBlaster);
 
@@ -189,6 +191,25 @@ public class PlayerController : MonoBehaviour
 
                 default:
                     break;
+            }
+        }
+
+        if (other.gameObject.tag == "enemy")
+        {
+            if (playerArmor)
+            {
+                playerArmor = false;
+                StartCoroutine("cooldownArmor");
+                Debug.Log("armor is hit");
+            }
+
+            else
+            {
+                playerHealth -= 25f;
+                canHit = false;
+                StartCoroutine("cooldownHit");
+                StartCoroutine("cooldownArmor");
+                Debug.Log("health is hit and armor is reset i hope");
             }
         }
     }
@@ -214,5 +235,17 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(dashCooldown);
         canDash = true;
+    }
+
+    IEnumerator cooldownHit()
+    {
+        yield return new WaitForSeconds(hitCooldown);
+        canHit = true;
+    }
+
+    IEnumerator cooldownArmor() // this is likely going to need to be changed later, as I want armor to come from parries and dodges
+    {
+        yield return new WaitForSeconds(armorCooldown);
+        playerArmor = true;
     }
 }
